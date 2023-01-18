4e4479262c3b63b6636e126938cc68eaf3640e59
MagUbas <magdalena.ubas@gmail.com>
2023-01-18 14:56:39 +0100

    added scale change to chart

--- src/components/spendings/chart/chart.js
+++ src/components/spendings/chart/chart.js
@@ -3,16 +3,36 @@ import classes from "./chart.module.css";
 
 const Chart = (props) => {
   const [day, setDay] = useState("");
+  const [data, setData] = useState([]);
 
   useEffect(() => {
     const weekday = ["sun", "mond", "tues", "wed", "thu", "fri", "sat"];
     const d = new Date();
     setDay(weekday[d.getDay()]);
-  }, []);
+
+    const sizing = props.data.every((elem) => elem["amount"] < 140);
+    if (sizing) {
+      const newData = props.data.map((elem) => {
+        let newEl = { ...elem };
+        newEl["size"] = elem["amount"];
+        return newEl;
+      });
+      setData(newData);
+    } else {
+      const divisor =
+        Math.max(...props.data.map((elem) => elem["amount"])) / 100;
+      const newData = props.data.map((elem) => {
+        let newEl = { ...elem };
+        newEl["size"] = elem["amount"] / divisor;
+        return newEl;
+      });
+      setData(newData);
+    }
+  }, [props.data]);
 
   return (
     <div className={classes.chartBody}>
-      {props.data.map((elem) => {
+      {data.map((elem) => {
         return (
           <div className={classes.chartDay} key={elem["day"]}>
             <div className={classes.chartDayValue}>$ {elem["amount"]}</div>
@@ -20,7 +40,7 @@ const Chart = (props) => {
               className={`${classes.chartDayBar} ${
                 day === elem["day"] ? classes.chartDayBarActive : null
               }`}
-              style={{ height: `${elem["amount"] * 2}px` }}
+              style={{ height: `${elem["size"] * 2}px` }}
             ></div>
             <p>{elem["day"]}</p>
           </div>
